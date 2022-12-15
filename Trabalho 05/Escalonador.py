13dc5acd23ab9cda7489b75d56a5480534a57868
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2022-12-15 15:59:33 -0300

    Adicionado Prioridade

--- Trabalho 05/Escalonador.py
+++ Trabalho 05/Escalonador.py
@@ -7,31 +7,39 @@ def add_processo(id, tempo, prioridade):
     
 def RoundRobin(processo, quantum):
     finalizados = len(processo)
-    
-    while (finalizados >= 0):
+    while (finalizados > 0):
         i = 0
         while(i < len(processo)):
             temp = processo[i][1]
             processo[i][1] -= quantum
             processo[i][3] = temp - abs(processo[i][1])
-            #print(f"Processo {processo[i][0]}\tTempo Restante: {processo[i][1]}\tExecução: {abs(processos[i][3])}")
             if(processo[i][1] > 0):
                 print(f"Processo {processo[i][0]}\tExecução: {processo[i][3]}")
             elif (processo[i][1] <= 0):
                 finalizados -= 1
                 print(f"Processo {processo[i][0]}\tExecução: {abs(processo[i][3])}*")
-                #print(f"Processo {processo[i][0]} foi finalizado")
                 processo.pop(i)
             i += 1
-        if (finalizados == 0): break
 
 
-def prioridade(processo, quantum):
+def RRprioridade(processo, quantum):
     processos_ordenados = sorted(processo, key=itemgetter(2))
     processo = processos_ordenados
-    #print(processos_ordenados)
     RoundRobin(processo, quantum)
+
+
+def Prioridade(processo, quantum):
+    Priori_alta = []
+    Priori_baixa = []
     
+    for proc in processo:
+        if proc[2] <= 5: Priori_baixa.append(proc) 
+        else: Priori_alta.append(proc)
+    
+    print(f"\tProcessos com prioridade alta: ")
+    RoundRobin(Priori_alta, quantum)
+    print(f"\tProcessos com prioridade baixa: ")
+    RoundRobin(Priori_baixa, quantum)
 
 #-----------------------MAIN-----------------------#
 processos = []
@@ -44,5 +52,17 @@ add_processo("P5", 250, 10)
 add_processo("P6", 300, 7)
 add_processo("P7", 40, 2)
 
-#RoundRobin(processos, 11)
-prioridade(processos, 11)
+print(f"\tEscalonador Round Robin: ")
+RoundRobin(processos, 11)
+
+
+add_processo("P1", 200, 5)
+add_processo("P2", 120, 9)
+add_processo("P3", 400, 0)
+add_processo("P4", 90, 4)
+add_processo("P5", 250, 10)
+add_processo("P6", 300, 7)
+add_processo("P7", 40, 2)
+print(f"\n\tEscalonador com Prioridade: ")
+#RRprioridade(processos, 11)
+Prioridade(processos, 11)
